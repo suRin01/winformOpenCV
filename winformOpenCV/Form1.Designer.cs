3cd0927fbc861676c20d6f970fb1f681096161f6
suRin01 <soullog77@gmail.com>
2020-03-19 18:31:55 +0900

    Initial commit

---
+++ winformOpenCV/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace winformOpenCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cameraList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.recordTargetDirectory = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.connectSerial = new System.Windows.Forms.Button();
+            this.loggingTargetDirectory = new System.Windows.Forms.FolderBrowserDialog();
+            this.REC = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(324, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(546, 412);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("D2Coding ligature", 9F);
+            this.button1.Location = new System.Drawing.Point(632, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cameraList
+            // 
+            this.cameraList.Font = new System.Drawing.Font("D2Coding ligature", 9F);
+            this.cameraList.FormattingEnabled = true;
+            this.cameraList.Location = new System.Drawing.Point(324, 61);
+            this.cameraList.Name = "cameraList";
+            this.cameraList.Size = new System.Drawing.Size(302, 22);
+            this.cameraList.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("D2Coding ligature", 15F);
+            this.label1.Location = new System.Drawing.Point(320, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Live Camera";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(9, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(306, 412);
+            this.listBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(9, 63);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(184, 20);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("D2Coding ligature", 15F);
+            this.label2.Location = new System.Drawing.Point(5, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Printer List";
+            // 
+            // connectSerial
+            // 
+            this.connectSerial.Font = new System.Drawing.Font("D2Coding ligature", 9F);
+            this.connectSerial.Location = new System.Drawing.Point(199, 61);
+            this.connectSerial.Name = "connectSerial";
+            this.connectSerial.Size = new System.Drawing.Size(116, 23);
+            this.connectSerial.TabIndex = 8;
+            this.connectSerial.Text = "Connect";
+            this.connectSerial.UseVisualStyleBackColor = true;
+            // 
+            // REC
+            // 
+            this.REC.Font = new System.Drawing.Font("D2Coding ligature", 9F);
+            this.REC.Location = new System.Drawing.Point(754, 61);
+            this.REC.Name = "REC";
+            this.REC.Size = new System.Drawing.Size(116, 21);
+            this.REC.TabIndex = 2;
+            this.REC.Text = "REC start";
+            this.REC.UseVisualStyleBackColor = true;
+            this.REC.Click += new System.EventHandler(this.RECstart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 513);
+            this.Controls.Add(this.connectSerial);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cameraList);
+            this.Controls.Add(this.REC);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "3D Printer Remote Management Program v0.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cameraList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog recordTargetDirectory;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connectSerial;
+        private System.Windows.Forms.FolderBrowserDialog loggingTargetDirectory;
+        private System.Windows.Forms.Button REC;
+    }
+}
+
---
+++ winformOpenCV/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenCvSharp;
+using Accord.Video.DirectShow;
+using System.Runtime.CompilerServices;
+
+namespace winformOpenCV
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        int isCameraRunning = 0;
+        int isRecRunning = 0;
+        OpenCvSharp.VideoWriter writer = new OpenCvSharp.VideoWriter();
+        int codec = OpenCvSharp.VideoWriter.FourCC('M', 'J', 'P', 'G');
+        double fps = 20.0;
+        string filename = "./live.avi";
+        
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            FilterInfoCollection FilterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in FilterInfoCollection)
+                cameraList.Items.Add(filterInfo.Name);
+            cameraList.SelectedIndex = 0;
+        }
+        public string GetDateTime()//시간 데이터 저장함수
+
+        {
+            DateTime NowDate = DateTime.Now;
+            return NowDate.ToString("yyyy-MM-dd HH:mm:ss") + ":" + NowDate.Millisecond.ToString("000");
+        }
+        public void stopCamera(VideoCapture cam, VideoWriter videoWriter)
+        {
+            if (cam.IsOpened())
+            {
+                cam.Release();
+                if (camera.IsAlive)
+                {
+                    camera.Abort();
+                    camera.Join();
+                }
+                videoWriter.Release();
+            }
+
+        }
+        public void startCamera(VideoCapture cam, int cameraIndex)
+        {
+            if (cam.IsDisposed)
+            {
+                cam.Release();
+
+
+            }
+        }
+
+        private void CaptureCamera()
+        {
+
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            stopCamera(capture, writer);
+        }
+
+        private void CaptureCameraCallback()
+        {
+            frame = new Mat();
+            capture = new VideoCapture();
+            capture.Open(0);
+
+            while (isCameraRunning == 1)
+            {
+                capture.Read(frame);
+                if (!frame.Empty())
+                {
+                    image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame);
+                    pictureBox1.Image = image;
+                }
+                if(isRecRunning == 1)
+                {
+                    writer.Write(frame);
+                }
+                image = null;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("Start"))
+            {
+                CaptureCamera();
+                button1.Text = "Stop";
+                isCameraRunning = 1;
+            }
+            else
+            {
+                if (capture.IsOpened())
+                {
+                    capture.Release();
+                }
+
+                button1.Text = "Start";
+                isCameraRunning = 0;
+            }
+        }
+
+        private void RECstart_Click(object sender, EventArgs e)
+        {
+            if (REC.Text.Equals("REC start"))
+            {
+                string recSaveTargetDirectory = @"./REC/" + DateTime.Now.ToString("yyyy-MM-dd");
+                if (!System.IO.File.Exists(recSaveTargetDirectory))
+                {
+                    System.IO.Directory.CreateDirectory(recSaveTargetDirectory);
+                }
+                string fileName = recSaveTargetDirectory + "/" + "rec.avi";
+                REC.Text = "REC stop";
+                isRecRunning = 1;
+                writer.Open(fileName, codec, fps, frame.Size(), true);
+            }
+            else
+            {
+                if (capture.IsOpened())
+                {
+                    capture.Release();
+                    writer.Release();
+                }
+
+                REC.Text = "REC start";
+                isRecRunning = 0;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
