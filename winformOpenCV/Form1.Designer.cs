3e4dfca79fbdd7cf9dce646b505a46941a96a4a0
suRin01 <soullog77@gmail.com>
2020-03-24 00:38:07 +0900

    logging function added

--- winformOpenCV/Form1.Designer.cs
+++ winformOpenCV/Form1.Designer.cs
@@ -35,12 +35,12 @@
             this.cameraList = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.recordTargetDirectory = new System.Windows.Forms.FolderBrowserDialog();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.connectSerial = new System.Windows.Forms.Button();
             this.loggingTargetDirectory = new System.Windows.Forms.FolderBrowserDialog();
             this.REC = new System.Windows.Forms.Button();
+            this.serialBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,18 +48,20 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(324, 93);
+            this.pictureBox1.Location = new System.Drawing.Point(370, 116);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(546, 412);
+            this.pictureBox1.Size = new System.Drawing.Size(624, 514);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("D2Coding ligature", 9F);
-            this.button1.Location = new System.Drawing.Point(632, 61);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button1.Location = new System.Drawing.Point(722, 76);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 22);
+            this.button1.Size = new System.Drawing.Size(133, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -67,85 +69,89 @@
             // 
             // cameraList
             // 
-            this.cameraList.Font = new System.Drawing.Font("D2Coding ligature", 9F);
+            this.cameraList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.cameraList.FormattingEnabled = true;
-            this.cameraList.Location = new System.Drawing.Point(324, 61);
+            this.cameraList.Location = new System.Drawing.Point(370, 76);
+            this.cameraList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cameraList.Name = "cameraList";
-            this.cameraList.Size = new System.Drawing.Size(302, 22);
+            this.cameraList.Size = new System.Drawing.Size(345, 23);
             this.cameraList.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("D2Coding ligature", 15F);
-            this.label1.Location = new System.Drawing.Point(320, 23);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(366, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 23);
+            this.label1.Size = new System.Drawing.Size(123, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "Live Camera";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(9, 93);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(306, 412);
-            this.listBox1.TabIndex = 6;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(9, 63);
+            this.comboBox2.Location = new System.Drawing.Point(10, 79);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(184, 20);
+            this.comboBox2.Size = new System.Drawing.Size(210, 23);
             this.comboBox2.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("D2Coding ligature", 15F);
-            this.label2.Location = new System.Drawing.Point(5, 23);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(6, 29);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 23);
+            this.label2.Size = new System.Drawing.Size(103, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Printer List";
             // 
             // connectSerial
             // 
-            this.connectSerial.Font = new System.Drawing.Font("D2Coding ligature", 9F);
-            this.connectSerial.Location = new System.Drawing.Point(199, 61);
+            this.connectSerial.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.connectSerial.Location = new System.Drawing.Point(227, 76);
+            this.connectSerial.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.connectSerial.Name = "connectSerial";
-            this.connectSerial.Size = new System.Drawing.Size(116, 23);
+            this.connectSerial.Size = new System.Drawing.Size(133, 29);
             this.connectSerial.TabIndex = 8;
             this.connectSerial.Text = "Connect";
             this.connectSerial.UseVisualStyleBackColor = true;
             // 
             // REC
             // 
-            this.REC.Font = new System.Drawing.Font("D2Coding ligature", 9F);
-            this.REC.Location = new System.Drawing.Point(754, 61);
+            this.REC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.REC.Location = new System.Drawing.Point(862, 76);
+            this.REC.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.REC.Name = "REC";
-            this.REC.Size = new System.Drawing.Size(116, 21);
+            this.REC.Size = new System.Drawing.Size(133, 26);
             this.REC.TabIndex = 2;
             this.REC.Text = "REC start";
             this.REC.UseVisualStyleBackColor = true;
             this.REC.Click += new System.EventHandler(this.RECstart_Click);
             // 
+            // serialBox
+            // 
+            this.serialBox.Location = new System.Drawing.Point(10, 116);
+            this.serialBox.Multiline = true;
+            this.serialBox.Name = "serialBox";
+            this.serialBox.Size = new System.Drawing.Size(350, 513);
+            this.serialBox.TabIndex = 9;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 513);
+            this.ClientSize = new System.Drawing.Size(1006, 641);
+            this.Controls.Add(this.serialBox);
             this.Controls.Add(this.connectSerial);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cameraList);
             this.Controls.Add(this.REC);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "3D Printer Remote Management Program v0.1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -164,12 +170,12 @@
         private System.Windows.Forms.ComboBox cameraList;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.FolderBrowserDialog recordTargetDirectory;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button connectSerial;
         private System.Windows.Forms.FolderBrowserDialog loggingTargetDirectory;
         private System.Windows.Forms.Button REC;
+        private System.Windows.Forms.TextBox serialBox;
     }
 }
 
--- winformOpenCV/Form1.cs
+++ winformOpenCV/Form1.cs
@@ -10,20 +10,30 @@ using System.Windows.Forms;
 using System.Threading;
 using OpenCvSharp;
 using Accord.Video.DirectShow;
+using System.IO.Ports;
 using System.Runtime.CompilerServices;
 
 namespace winformOpenCV
 {
     public partial class Form1 : Form
     {
+        //Variable for logger
+        System.IO.StreamWriter file;
+    
+        //Variable for OpenCV
         VideoCapture capture = new VideoCapture();
         VideoWriter writer = new VideoWriter();
         private Thread cameraThread;
         Mat frame;
         int selectedCameraIndex = 0;
-        
 
-        
+        //Variable for Serial IO
+        SerialPort my_serial = new SerialPort();        // 시리얼 포트 변수 생성
+        delegate void SetTextCallBack(string str);      // Callback 함수
+
+
+
+        //Form construction, onLoad, destruction 
         public Form1()
         {
             InitializeComponent();
@@ -35,11 +45,45 @@ namespace winformOpenCV
             foreach (FilterInfo filterInfo in FilterInfoCollection)
                 cameraList.Items.Add(filterInfo.Name);
             cameraList.SelectedIndex = 0;
+
+
+            file = new System.IO.StreamWriter(initLogPath());
+            my_serial.DataReceived += new SerialDataReceivedEventHandler(serial_DataReceived);
         }
         private void Form1_FormClosing(object sender, EventArgs e)
         {
             stopCamera();
         }
+
+        //Dealing sercial IO Interupt 
+        private string initLogPath()
+        {
+            string recSaveTargetDirectory = @"./Log/" + DateTime.Now.ToString("yyyy-MM-dd");
+            if (!System.IO.File.Exists(recSaveTargetDirectory))
+            {
+                System.IO.Directory.CreateDirectory(recSaveTargetDirectory);
+            }
+            return recSaveTargetDirectory + "/" + DateTime.Now.ToString("HHmmss") + ".log";
+        }
+
+        private void serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int i_recv_size = my_serial.BytesToRead;
+            byte[] b_tmp_buf = new byte[i_recv_size];
+            string recv_str = "";
+
+            my_serial.Read(b_tmp_buf, 0, i_recv_size);
+            recv_str = Encoding.Default.GetString(b_tmp_buf);
+            this.BeginInvoke(new SetTextCallBack(display_data), new object[] { recv_str });
+        }
+
+        private void display_data(string str)
+        {
+            this.serialBox.AppendText(str);
+            file.WriteLine(str);
+        }
+
+        //Dealing Camera Data
         public void startCamera(VideoCapture cam, int cameraIndex)
         {
             if (cam.IsDisposed)
